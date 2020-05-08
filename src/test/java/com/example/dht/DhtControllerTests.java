12cc915b9eda3a77f391b3b2451d2a0c460841ee
gcm <59884013+gcm256@users.noreply.github.com>
2020-05-08 02:25:10 -0700

    Automated tests, no need to start server manually. Run command = ./mvnw test

--- src/test/java/com/example/dht/DhtControllerTests.java
+++ src/test/java/com/example/dht/DhtControllerTests.java
@@ -1,20 +1,24 @@
 package com.example.dht;
 
+import com.example.dht.model.DhtModel;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest
 public class DhtControllerTests {
     //@Autowired
     private RestTemplate restTemplate;
 
-    //@LocalServerPort
-    private int port = 8080;
+    @LocalServerPort
+    private int port;
 
     @Test
     public void testHello() throws Exception {
@@ -25,5 +29,44 @@ public class DhtControllerTests {
                                                             + "/hello/DummyName"
                                               , String.class))
                 .isEqualTo("Hello, DummyName!\n");
+    }
+
+    @Test
+    public void testGetEmptyDht() throws Exception {
+        restTemplate = new RestTemplate();
+        Assertions.assertThat(this.restTemplate
+                                      .getForObject("http://localhost:"
+                                                            + port
+                                                            + "/get/key1"
+                                              , String.class))
+                .isNull();
+    }
+
+    //@Test
+    public void testGetAfterSet() throws Exception {
+        restTemplate = new RestTemplate();
+        String key = "key1", value = "value1";
+        String setUri = "http://localhost:" + port + "/set/{keyName}";
+        String getUri = "http://localhost:" + port + "/get/"+key;
+        this.restTemplate.exchange(setUri, HttpMethod.POST, getHttpEntity(value), String.class, key);
+        Assertions.assertThat(this.restTemplate.getForObject(getUri, String.class))
+                .isEqualTo(value);
+    }
+
+    @Test
+    public void testSet() throws Exception {
+        restTemplate = new RestTemplate();
+        String key = "key1", value = "value1";
+        String setUri = "http://localhost:" + port + "/set/{keyName}";
+        Assertions.assertThat(this.restTemplate
+                                      .exchange(setUri,
+                                              HttpMethod.POST,
+                                              getHttpEntity(value), String.class, key)
+                                      .getBody()).isEqualTo("OK\n");
+    }
+
+    private static HttpEntity<String> getHttpEntity(String body) {
+        HttpHeaders headers = new HttpHeaders();
+        return new HttpEntity<>(body, headers);
     }
 }
